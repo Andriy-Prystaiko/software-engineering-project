a339d76b06de9a082db6c083b40affda05b279ad
Tyler Supersad <96035297+tyeborg@users.noreply.github.com>
2022-02-27 16:01:51 +0000

    Beginning Javascript Lab - Week 03
    Started working with Javascript Code (learning basics).

---
+++ labs/week-03/js-conditionals/script4.js
@@ -0,0 +1,9 @@
+// Use IF statements and a FOR loop to output numbers 1 to 20.
+for (var i = 0; i < 21; i++) {
+    if (i == 0) {
+        continue;
+    }
+    else {
+        document.write(i + "\n")
+    }
+}
---
+++ labs/week-03/more-js-code/script2.js
@@ -0,0 +1,7 @@
+var name = "Tyler";
+var age = 21;
+var teacher = false; // Notice all lowercase true
+
+document.write('Name: ' + name + '<br>');  // + is used for string concatent
+document.write('Age: ' + age + '<br>');  // Try using + with another number
+document.write('Is teacher? ' + teacher);
