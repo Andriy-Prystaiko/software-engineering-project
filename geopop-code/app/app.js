bc66318f29185c5c2958c3538674bd97e2196557
Tyler Supersad <supersadtyler@gmail.com>
2022-03-24 23:07:11 +0000

    Enabling to Pull Country Data from Database

--- geopop-code/app/app.js
+++ geopop-code/app/app.js
@@ -1,5 +1,5 @@
 // Bring in our express module.
-const express = require("express");
+const express = require('express');
 // Bring in the path module.
 const path = require('path');
 
@@ -12,11 +12,33 @@ app.set('views', './app/views');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+app.use(express.urlencoded({ extended: true}));
+
+// Get our models
+const { Country } = require('./models/country');
 
 // Use a get request to the HOME page.
-app.get("/", function(req, res){
+app.get('/', function(req, res){
     // Send the 'index' view to the browser.
-    res.render("index");
+    res.render('index');
+});
+
+app.get('/country-report', function(req, res) {
+    // When initally rendering the 'country' page, we
+    // will need a blank country object to avoid errors
+    var country = new Country('');
+    // Render in the country page through the country.pug view
+    // Also, enable access to student values within country.pug
+    res.render('country', {student:student});
+});
+
+app.post('/received-response', async function (req, res) {
+    // Get the submitted value from the user
+    var userResponse = req.body.country;
+    var country = new Country(countryName);
+    await country.getCountryDetails();
+    // Render in the index page once more along with 'country' values
+    res.render('country', {country:country});
 });
 
 // Start the server on Port 3000...
