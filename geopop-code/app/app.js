f2227e6a88e5634e3b68a4478dbc6ef724afd2d2
Tyler Supersad <supersadtyler@gmail.com>
2022-04-15 02:04:58 +0100

    Started working on 'City' report page

--- geopop-code/app/app.js
+++ geopop-code/app/app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
+global.document = {
+    execCommand() {}
+};
+
 // Assign a variable to the express function
 var app = express();
 app.use(cookieParser());
@@ -21,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Get the models
 const { Country } = require('./models/country');
+const { City } = require('./models/city');
 
 // Use a get request to the HOME page.
 app.get("/", function(req, res){
@@ -39,7 +44,105 @@ function isEmpty(obj) {
     return true;
 }
 
-app.get("/country-report", async function(req, res) {
+function activeCityResponse(city, continent, region, country, district) {
+    var inputs = [city, continent, region, country, district];
+    
+    for (var input of inputs) {
+        if (input != null) {
+            return input;
+        }
+    }
+}
+
+app.get("/city", async function(req, res) {
+    // Create an objects for the initial display items
+    var cityDisplayText = new City(null);
+
+    // Initialize a list to store all the dictionary values from 'results'
+    var cityList = [];
+    // Initialize lists that will be dependent on the value of the cityInput
+    var continentList = [];
+    var regionList = [];
+    var countryList = [];
+    var districtList = [];
+
+    // Call upon the allCityNames() function to receive a list of all cities
+    await cityDisplayText.allCityNames();
+    // Initialize a variable to store all city names
+    var cityDisplayList = cityDisplayText.data;
+    // Add all cities within the cityList
+    for (var cities of cityDisplayList) {
+        cityList.push(cities["Name"]);
+    }
+
+    // Call upon the allContinentsGrouped() function to receive a list of all 
+    // grouped continents 
+    await cityDisplayText.allContinentsGrouped();
+    // Initialize a variable to store all grouped continents
+    var continentDisplayList = cityDisplayText.data;
+    // Add all grouped continents within the continentList
+    for (var continent of continentDisplayList) {
+        continentList.push(continent["Continent"]);
+    }
+
+    // Call upon the allRegionsGrouped() function to receive a list of all
+    // grouped regions
+    await cityDisplayText.allRegionsGrouped();
+    // Initialize a variable to store all grouped regions
+    var regionDisplayList = cityDisplayText.data;
+    // Add all grouped regions within the regionList
+    for (var region of regionDisplayList) {
+        regionList.push(region["Region"]);
+    }
+
+    // Call upon the allCountries() function to receive a list of all countries
+    await cityDisplayText.allCountries();
+    // Initialize a variable to store all countries
+    var countryDisplayList = cityDisplayText.data;
+    // Add all the countries within the countryList
+    for (var country of countryDisplayList) {
+        countryList.push(country["Country"]);
+    }
+
+    // Call upon the allCountries() function to receive a list of all countries
+    await cityDisplayText.allDistrictsGrouped();
+    // Initialize a variable to store all countries
+    var districtDisplayList = cityDisplayText.data;
+    // Add all the countries within the countryList
+    for (var district of districtDisplayList) {
+        districtList.push(district["District"]);
+    }
+
+    // Initialize the cookies that hold an object with all 
+    // the cookie key-values pairs
+    const cookies = req.cookies;
+    // Declare a variable to store the 'response' value (name of cookie)
+    var cityInput = cookies['cityCityInput'];
+    var continentInput = cookies['continentCityInput'];
+    var regionInput = cookies['regionCityInput'];
+    var countryInput = cookies['countryCityInput'];
+    var districtInput = cookies['districtCityInput'];
+
+    var userInput = activeCityResponse(cityInput, continentInput, 
+        regionInput, countryInput, districtInput);
+
+    // Create an object for the city table data
+    var city = new City(userInput);
+    
+    if (userInput == cityInput) {
+        if (cityInput == "Select All" || cityInput == null) {
+            await city.selectAllCities();
+        } else {
+            //await city.selectSpecificCity();
+        }
+    }
+
+    // Render the 'city' page and pass some data into the page
+    res.render("city", {city:city, cityList:cityList, continentList:continentList, 
+        regionList:regionList, countryList:countryList, districtList:districtList});
+});
+
+app.get("/country", async function(req, res) {
     // Initialize the cookies that hold an object with all 
     // the cookie key-values pairs
     const cookies = req.cookies;
@@ -68,7 +171,7 @@ app.get("/country-report", async function(req, res) {
         // will need a blank country object to avoid errors
         countryResponse = '';
         country = new Country(countryResponse);
-        await country.getAllInitially();
+        //await country.getAllInitially();
     } else {
         country = new Country(countryResponse);
         // Get the details that correspond with the inputted country
@@ -95,8 +198,23 @@ app.post('/received-response', function (req, res) {
     res.cookie('rankResponse', params.rank);
     
     // After setting the cookie, redirect to the 'country-report' endpoint
-    res.redirect('/country-report');
+    res.redirect('/country');
 });
+
+app.post('/city-response'), function (req, res) {
+    // Get the submitted value from the user
+    var params = req.body;
+    
+    // Set a cookie based on each text-box input on the city page
+    res.cookie('cityCityInput', params.cityCityInput);
+    res.cookie('continentCityInput', params.continentCityInput);
+    res.cookie('regionCityInput', params.regionCityInput);
+    res.cookie('countryCityInput', params.countryCityInput);
+    res.cookie('districtCityInput', params.districtCityInput);
+
+    // After setting the cookie, redirect to the 'country-report' endpoint
+    res.redirect('/city');
+}
 
 // Start the server on Port 3000...
 app.listen(3000, function(){
---
+++ geopop-code/app/models/city.js
@@ -0,0 +1,143 @@
+// Get the functions in the db.js file to use
+const db = require('./../services/db');
+
+class City {
+    // Store all values within an array
+    data = []
+    // The User Choice Response
+    response;
+
+    // Set the Country Name as the constructor
+    constructor(response) {
+        this.response = response;
+    }
+
+    async allCityNames() {
+        var sql = "SELECT city.Name AS cityName \
+        FROM city";
+        
+        const results = await db.query(sql);
+
+        // Iterate throughout the results (BinaryRow)
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            // Coordinate the respective values with their correct key
+            rowDict["Name"] = row.cityName;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive all Continents Grouped Together
+    async allContinentsGrouped() {
+        // Construct a query to receive all the continents from database
+        var sql = "SELECT Continent FROM country GROUP BY Continent";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the continents within the continentList
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["Continent"] = row.Continent;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive all Regions Grouped Together
+    async allRegionsGrouped() {
+        // Construct a query to receive all the regions from database
+        var sql = "SELECT Region FROM country GROUP BY Region";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the continents within the data list
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["Region"] = row.Region;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive all Countries Grouped Together
+    async allCountries() {
+        // Construct a query to receive all the countries from database
+        var sql = "SELECT country.Name AS Country FROM country ORDER BY Country ASC";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the countries within the data list
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["Country"] = row.Country;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive all Districts Grouped Together
+    async allDistrictsGrouped() {
+        // Construct a query to receive all the grouped districts from the database
+        var sql = "SELECT District FROM city GROUP BY District";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the districts within the data list
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["District"] = row.District;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive City name, country, district, and population
+    async selectAllCities() {
+        // Construct a query to receive the City name, country, district and population from the database
+        var sql = "SELECT city.Name AS Name, country.Name AS Country, \
+        city.District AS District, city.Population AS Population FROM city, country \
+        WHERE city.CountryCode = country.Code ORDER BY Name ASC";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the districts within the data list
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["Name"] = row.Name;
+            rowDict["Country"] = row.Country;
+            rowDict["District"] = row.District;
+            rowDict["Population"] = row.Population;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+    // Create a function to receive City name, country, district, and population
+    async selectSpecificCity() {
+        // Construct a query to receive the City name, country, district and population from the database
+        var sql = "SELECT city.Name AS Name, country.Name AS Country, \
+        city.District AS District, city.Population AS Population FROM city, country \
+        WHERE city.CountryCode = country.Code ORDER BY Country DESC";
+        // Receive the results and store them inside a variable
+        const results = await db.query(sql);
+        // Store all the districts within the data list
+        for(var row of results) {
+            // Initialize a dictionary to store values
+            var rowDict = {};
+            rowDict["Name"] = row.Name;
+            rowDict["Country"] = row.Country;
+            rowDict["District"] = row.District;
+            rowDict["Population"] = row.Population;
+            // For each iteration, add the new rowDict into the data array
+            this.data.push(rowDict);
+        }
+    }
+
+
+}
+
+module.exports = {
+    City,
+}
